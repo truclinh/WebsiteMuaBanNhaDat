cdddc9696be4406179fae337ef9c224e02559cb7
Linh Thai <thaitruclinh151296@gmail.com>
2018-04-29 16:57:16 +0700

    29/4/2018 4.57PM

--- WebsiteMuaBanNhaDat1/WebsiteMuaBanNhaDat1/Controllers/QuanLyController.cs
+++ WebsiteMuaBanNhaDat1/WebsiteMuaBanNhaDat1/Controllers/QuanLyController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebsiteMuaBanNhaDat1.Models;
 namespace WebsiteMuaBanNhaDat1.Controllers
 {
     public class QuanLyController : Controller
     {
+        MuaBanNhaDatEntities db = new MuaBanNhaDatEntities();
         // GET: QuanLy
         public ActionResult Index()
         {
-            return View();
+            var da = db.LoaiHinh.OrderBy(n => n.ma_loaihinh).ToList();
+            return View(da);
+        }
+        //-------------------------------------- Menu chính
+        public ActionResult MenuPartial()
+        {
+            var da = db.LoaiHinh.OrderBy(n => n.ma_loaihinh).ToList();
+            return PartialView("MenuPartial", da);
+        }
+        //--------------------------------------Menu item
+        public ActionResult MenuItemPartial(int? ma)
+        {
+            var da = db.NoiDungLoaiHinh.Where(n => n.ma_loaihinh == ma).ToList().OrderBy(n => n.ten_ndloaihinh);
+            return PartialView("MenuItemPartial", da);
         }
     }
 }
